83ba2f895fed39a8922055a36f8bbb7e0ed87e19
Geoffery Joseph <geofferyjoseph1@gmail.com>
2020-09-20 15:36:56 +0100

    Added Contactme modal

--- project/core/settings/settings_prod.py
+++ project/core/settings/settings_prod.py
@@ -4,14 +4,21 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'handsfreeyoutube.com']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'handsfreeyoutube_db',
+#         'USER': 'project_admin',
+#         'PASSWORD': 'project_admin_password_#001',
+#         'HOST': '162.214.124.233',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'handsfreeyoutube_db',
-        'USER': 'project_admin',
-        'PASSWORD': 'project_admin_password_#001',
-        'HOST': '162.214.124.233',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
